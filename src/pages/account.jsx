58ae9034aace0a76fcde33d82faeb922223521e0
Alex Leonetti <alexleonetti.dev@gmail.com>
2020-07-13 12:04:25 -0700

    Added reach/router and prop-types, as well as Account page
    Account page uses reach router to display different routes created with gatsby-node.js

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Router } from '@reach/router';
+import { Link } from 'gatsby';
 // import PropTypes from 'prop-types';
 
+const Home = () => <p>Account Home</p>;
+const Settings = () => <p>Settings</p>;
+const Billing = () => <p>Billing</p>;
+
 const Account = () => (
-  <div>
-    This route will be protected
-  </div>
+  <>
+    <nav>
+      <Link to="/account">Home</Link>
+      <Link to="/account/settings">Settings</Link>
+      <Link to="/account/billing">Billing</Link>
+    </nav>
+    <Router>
+      <Home path="/account" />
+      <Settings path="/account/settings" />
+      <Billing path="/account/billing" />
+    </Router>
+  </>
 );
-
 Account.propTypes = {
-
 };
-
 export default Account;
